776dbef8a2b63ff87ea8892352f5851d6038fff0
LuizMqs <luizantmarquesjr@gmail.com>
2022-06-08 09:27:33 -0300

    indice

--- Body-mass/script.js
+++ Body-mass/script.js
@@ -6,6 +6,29 @@ document.getElementById("button").addEventListener("click", function () {
     if (!isNaN(peso) && !isNaN(altura)) {
         resultado = peso / (altura * altura)
         document.querySelector("h3").innerHTML = resultado
+        if (resultado <= 18.5) {
+            document.querySelector("h4").innerHTML = "Abaixo do peso"
+        }
+
+        else if (resultado > 18.5 && resultado <= 24.9) {
+            document.querySelector("h4").innerHTML = "Peso normal"
+        }
+
+        else if (resultado > 24.9 && resultado <= 29.9) {
+            document.querySelector("h4").innerHTML = "Sobrepeso"
+        }
+
+        else if (resultado > 29.9 && resultado <= 34.9) {
+            document.querySelector("h4").innerHTML = "Obesidade grau I"
+        }
+
+        else if (resultado > 34.9 && resultado <= 39.9) {
+            document.querySelector("h4").innerHTML = "Obesidade grau II"
+        }
+
+        else {
+            document.querySelector("h4").innerHTML = "Obesidade grau III"
+        }
     }
 
     else {
